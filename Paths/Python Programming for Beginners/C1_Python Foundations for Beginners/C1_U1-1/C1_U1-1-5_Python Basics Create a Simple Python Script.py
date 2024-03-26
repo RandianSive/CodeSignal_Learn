2d73e4fe6011803a10e86800212bd68a9ae2b73a
RandianSive <randy11111w@gmail.com>
2024-03-26 19:37:09 +0800

    新增檔案

---
+++ Paths/Python Programming for Beginners/C1_Python Foundations for Beginners/C1_U1-1/C1_U1-1-5_Python Basics Create a Simple Python Script.py
@@ -0,0 +1,11 @@
+# Use Python syntax to complete this script.
+
+
+# TODO: Assign the number 25 to a variable named 'age'. Add a comment describing what you did.
+
+# This is age
+age = 25
+
+# TODO: Print a statement saying, "Yaaay! I did it!".
+
+print("Yaaay! I did it!")
